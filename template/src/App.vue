03c546221b752f5f36d0b14e5b2cc2c45fdd6b03
liwen <liwen@chehejia.com>
2018-09-21 17:06:32 +0800

    fix: 去掉logo

--- template/src/App.vue
+++ template/src/App.vue
@@ -20,6 +20,7 @@ export default {
 
 <style>
 #app {
+    color: black;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
